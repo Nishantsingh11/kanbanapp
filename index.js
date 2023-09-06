48a2380da99bc5329fc74f691e0f10f1a3011823
Nishant singh <38534128+Nishantsingh11@users.noreply.github.com>
2023-09-06 10:48:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use((err, req, res, next) => {
 // serve static assets if in production
 app.use(express.static(path.join(__dirname, "../build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build/index.html"));
+  res.sendFile(path.join(__dirname, "./build/index.html"));
 });
 
 // creating the endPoint for the adding the task
